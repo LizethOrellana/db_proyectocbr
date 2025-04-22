c9b00a329542aa42916e494a098d27ceaf470664
LizethOrellana <97999828+LizethOrellana@users.noreply.github.com>
2025-04-21 23:46:34 -0500

    cambio cors

--- src/main/java/com/proyectocbr/demo/web/CorsConfig.java
+++ src/main/java/com/proyectocbr/demo/web/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Permite CORS para todas las rutas
-                .allowedOrigins("http://ec2-18-218-153-234.us-east-2.compute.amazonaws.com:8080") // Permite solicitudes desde Angular (ajusta el puerto si es necesario)
+                .allowedOrigins("http://ec2-18-218-153-234.us-east-2.compute.amazonaws.com") // Permite solicitudes desde Angular (ajusta el puerto si es necesario)
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // Permite los métodos HTTP especificados
                 .allowedHeaders("*"); // Permite todos los encabezados
     }
